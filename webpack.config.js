bb74fc3f996f348f1f8df7d6189f33adc51e9985
Qwanii <busiki.ne@gmail.com>
2024-12-23 13:09:27 +0000

    server

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.TARGET = process.env.TARGET || 'web'
+
+
+console.log(`TARGET: ${process.env.TARGET}`);
+console.log(`NODE_ENV: ${process.env.NODE_ENV}`);
+
+const isProduction = process.env.NODE_ENV === 'production';
+const isDevelopment = process.env.NODE_ENV === 'development';
+const isWeb = process.env.TARGET === 'web';
+const isNode = process.env.TARGET === 'node';
+const target = process.env.TARGET;
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
 let config = {
+    name: target,
+    target: target,
     context: path.join(__dirname, '/src'), // Директория с исходным кодом приложения
-    entry: 'index.tsx', // Главный файл приложения
+    entry: `index.${target}.tsx`, // Главный файл приложения
     output: {
-      path: path.join(__dirname, 'dist'), // Куда делать оброку
+      path: path.join(__dirname, 'dist', target), // Куда делать оброку
       filename: '[name].js', // Шаблон для названия файлов
       clean: true, // Очистить ./dist перед сборкой
+      library: {
+        name: 'Root',
+        type: isNode ? 'commonjs2' : 'window',
+        export: 'default',
+      }
     },
     mode: process.env.NODE_ENV,
     resolve: {
@@ -43,26 +62,43 @@ let config = {
     },
     plugins: [
       new MiniCssExtractPlugin(), // Плагин для вытаскивания собранных стилей в отдельный файл
-      new HtmlWebPackPlugin({
-        template: './index.html',
-        filename: './index.html',
-        title: 'Simple SPA',
-        base: '/',
-      }),
+
+      // new HtmlWebPackPlugin({
+      //   template: './index.html',
+      //   filename: './index.html',
+      //   title: 'Simple SPA',
+      //   base: '/',
+      // }),
+
+
       new webpack.DefinePlugin({
         'process.env': {
           NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+          TARGET: JSON.stringify(process.env.TARGET),
+          IS_WEB: process.env.TARGET === 'web',
+          IS_NODE: process.env.TARGET === 'node',
         },
       }),
     ],
   }
 ;
 
-if (process.env.NODE_ENV === 'development') {
+if(isWeb) {
+  config.plugins.push(
+    new HtmlWebPackPlugin({
+      template: './index.html',
+      filename: './index.html',
+      title: 'Simple SPA',
+      base: '/'
+    })
+  )
+}
+
+if (isDevelopment && isWeb) {
   config.devtool = 'inline-source-map';
   config.devServer = {
     static: path.join(__dirname, 'dist'),
-    port: 8010,
+    port: 8011,
     historyApiFallback: true,
     proxy: [
       {
